0fe0fba64522d99cc6b6469786bb689151a0f743
isil kefal <kefalisil9@gmail.com>
2024-03-23 10:21:41 +0300

    delete import

--- src/pages/Certificates.jsx
+++ src/pages/Certificates.jsx
@@ -1,8 +1,8 @@
 import Layout from "../components/Layout/Layout";
-import {Container, Divider} from "@mui/material";
 import "../../src/styles/CertificatesStyles.css";
 import { info } from "../info/Info";
 import React, {useState} from "react";
+import {Container} from "@mui/material";
 
 const Certificates = () => {
     const [sliderData,setSliderData]=useState(info.certificates[0])
@@ -14,11 +14,11 @@ const Certificates = () => {
     return(
         <Layout>
             <Container maxWidth="md" className="containerCertificate">
-                <img src={sliderData.image} height="500" width="800"/>
+                <img src={sliderData.image} height="500" width="800" alt=""/>
                 <div>{sliderData.id}-) {sliderData.text} </div>
                 <div className="click">
                     {info.certificates.map((data, i) => (
-                        <img key={data.id} src={data.image} onClick={() => handleClick(i)} height="40" width="50"/>
+                        <img key={data.id} src={data.image} onClick={() => handleClick(i)} height="40" width="50" alt=""/>
                     ))}
                 </div>
             </Container>
